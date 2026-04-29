8ecd71b9edd0c40a8b258ec4831588d52fe8ac14
Hamza Sassi <hamza.sassi@esen.tn>
2026-04-29 15:31:44 +0200

    models.py aktualisieren

--- tickets/models.py
+++ tickets/models.py
@@ -9,7 +9,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
 from .utils import generate_pdf
-
+ 
 
 class Ticket(models.Model):
     STATUS_CHOICES = [
